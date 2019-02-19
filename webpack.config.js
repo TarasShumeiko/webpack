22e7500f52d4acf1dfa64f7624d169f2f17e4884
Rodion Lozovoi <rodionlozovoi@gmail.com>
2019-02-19 16:35:32 +0200

    adding webpack files

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
+module.exports = {
+    entry: [
+        './src/js/index.js',
+        './src/scss/style.scss'
+    ],
+    output: {
+        filename: './js/bundle.js'
+    },
+    devtool: "source-map",
+    module: {
+        rules: [{
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src/js'),
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(sass|scss)$/,
+                include: path.resolve(__dirname, 'src/scss'),
+                use: ExtractTextPlugin.extract({
+                    use: [{
+                            loader: "css-loader",
+                            options: {
+                                sourceMap: true,
+                                url: false
+                            }
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            },
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: './css/style.bundle.css',
+            allChunks: true,
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CopyWebpackPlugin([{
+                from: './src/fonts',
+                to: './fonts'
+            },
+            {
+                from: './src/favicon',
+                to: './favicon'
+            },
+            {
+                from: './src/uploads',
+                to: './uploads'
+            },
+            {
+                from: './src/img',
+                to: './img'
+            }
+        ]),
+        new ImageminPlugin({
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            disable: process.env.NODE_ENV !== 'production', // Disable during development
+            pngquant: {
+                quality: '95-100'
+            }
+        })
+    ]
+};
